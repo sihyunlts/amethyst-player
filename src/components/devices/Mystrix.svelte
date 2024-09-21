105c1ec19aca0511e93f1dca0d92bf21859b143c
203Null <null@203.io>
2024-09-21 01:05:30 -0400

    Underglow for Mystrix

--- src/components/devices/Mystrix.svelte
+++ src/components/devices/Mystrix.svelte
@@ -14,11 +14,12 @@
 
     export var deviceInfo: DeviceInfo = 
     {
-        dimension: [8, 8],
+        dimension: [10, 10],
         grid_dimension: [8, 8],
-        grid_offset: [0, 0],
-        layer_key: [[8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], 
-                    [-1, 7], [-1, 6], [-1, 5], [-1, 4], [-1, 3], [-1, 2], [-1, 1], [-1, 0]]
+        grid_offset: [1, 1],
+        layer_key: [[8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7],
+                    [7, 8], [6, 8], [5, 8], [4, 8], [3, 8], [2, 8], [1, 8], [0, 8],
+                    [-1, 7], [-1, 6], [-1, 5], [-1, 4], [-1, 3], [-1, 2], [-1, 1], [-1, 0]],
     }
     /** Get the clip path for the middle pads. */
     function getCornerRadius (x: number, y: number) {
@@ -44,7 +45,16 @@
     function getKeypadIndex(keyID: KeyID): number {
         if(Array.isArray(keyID))
         {
-            if(keyID[0] === 'c') keyID = deviceInfo.layer_key[keyID[1]]
+            if(keyID[0] === 'c') 
+            {keyID = deviceInfo.layer_key[keyID[1]];
+            }
+            else
+            {
+                keyID = [
+                    keyID[0] + deviceInfo.grid_offset[0],
+                    keyID[1] + deviceInfo.grid_offset[1]
+                ];
+            }
             return keyID[1] * 10 + keyID[0];
         }
         else
@@ -54,60 +64,183 @@
     }
 
 export function setColor(keyID: KeyID, color: Color) {
+        let underglow = false;
         if(Array.isArray(keyID))
         {
-            if(keyID[0] === 'c') keyID = deviceInfo.layer_key[keyID[1]];
-            keyID = [
-                keyID[0] + deviceInfo.grid_offset[0],
-                keyID[1] + deviceInfo.grid_offset[1]
-            ];
+            if(keyID[0] === 'c') 
+            {
+                keyID = deviceInfo.layer_key[keyID[1]];
+                underglow = true;
+            }
+            else if(keyID[0] == -1 || keyID[1] == -1 || keyID[0] == deviceInfo.grid_dimension[0] || keyID[1] == deviceInfo.grid_dimension[1]) 
+            {
+                underglow = true;
+            }
         }
         let index = getKeypadIndex(keyID)
         if(keyPads[index]) {
-            var screen_color = color.overlay(new Color(ColorType.RGB, [80, 80, 80]));
+            var screen_color;
+            if(underglow) {
+                screen_color = color.rgba();
+            } else {
+                screen_color = color.overlay(new Color(ColorType.RGB, [80, 80, 80]));
+            }
             keyPads[index].set_color(screen_color);           
         }
     }
 </script>
 
-<div class="lp-border">
-    <div class="lp-controls">
-        {#each Array(8) as _, y}
-            <div class="lp-controls-row">
-                {#each Array(8) as _2, x}
-                    <div class="lp-btn-parent">
-                        {#if (x >= 0 && x < 9) && (y >= 0 && y < 9)}
-                        <Keypad class="lp-normal-btn" style="clip-path: {getCornerRadius(x, y)};" deviceID={id} id={[x, y]} bind:this={keyPads[getKeypadIndex([x,y])]} keyPress={keyPress} keyRelease={keyRelease}/> 
-                        {/if}
+<div class="lp">
+  <div class="lp-underglow">
+            <div class="lp-underglow-row">
+                {#each Array(8) as _, x}
+                    <div class="lp-underglow-led-parent">
+                        <Keypad class="lp-underglow-led-btn" deviceID={id} id={[x, -1]} bind:this={keyPads[getKeypadIndex([x, -1])]} keyPress={keyPress} keyRelease={keyRelease}/> 
                     </div>
                 {/each}
             </div>
-        {/each}
+            <div style="display: flex; flex-direction: row; height:88%; justify-content: space-between;">
+                <div class="lp-underglow-column">
+                    {#each Array(8) as _, y}
+                        <div class="lp-underglow-led-parent">
+                            <Keypad class="lp-underglow-led-btn" deviceID={id} id={[-1, y]} bind:this={keyPads[getKeypadIndex([-1, y])]}/> 
+                        </div>
+                    {/each}
+                </div>
+    
+                <div class="lp-underglow-column">
+                    {#each Array(8) as _, y}
+                    <div class="lp-underglow-led-parent">
+                            <Keypad class="lp-underglow-led-btn" deviceID={id} id={[8, y]} bind:this={keyPads[getKeypadIndex([8, y])]}/> 
+                    </div>
+                    {/each}
+                </div>
+            </div>
+    
+            <div class="lp-underglow-row">
+                {#each Array(8) as _, x}
+                    <div class="lp-underglow-led-parent">
+                        <Keypad class="lp-underglow-led-btn" deviceID={id} id={[x, 8]} bind:this={keyPads[getKeypadIndex([x, 8])]}/> 
+                    </div>
+                {/each}
+            </div>
+        </div> 
+    <div class="lp-border">
+        
+        <div class="lp-controls">
+            {#each Array(8) as _, y}
+                <div class="lp-controls-row">
+                    {#each Array(8) as _2, x}
+                        <div class="lp-btn-parent">
+                            {#if (x >= 0 && x < 9) && (y >= 0 && y < 9)}
+                            <Keypad class="lp-normal-btn" style="clip-path: {getCornerRadius(x, y)};" deviceID={id} id={[x, y]} bind:this={keyPads[getKeypadIndex([x,y])]} keyPress={keyPress} keyRelease={keyRelease}/> 
+                            {/if}
+                        </div>
+                    {/each}
+                </div>
+            {/each}
+        </div>
     </div>
 </div>
 
 <style lang="scss">
-    .lp-border {
-        background: rgb(20, 20, 20);
-        border: 2px solid rgb(120, 120, 120);
-        border-radius: 3%;
-
+    .lp {
         position: relative;
 
         width: 100%;
         aspect-ratio: 1/1;
 
-
+        z-index: 1;
     }
 
+    .lp-border {
+        background-color: rgb(20, 20, 20);
+        border: 2px solid rgb(120, 120, 120);
+        border-radius: 3%;
+
+        position: absolute;
+
+        width: 100%;
+        aspect-ratio: 1/1;
+
+        z-index: 2;
+    }
+
+
+
+    .lp-underglow {
+        position: absolute;
+        top: -3%;
+        left: -3%;
+        height: 106%;
+        width: 106%;
+        // background-color: rgb(20, 20, 20);
+
+        // display: flex;
+        // flex-direction: column;
+        filter: blur(20px) saturate(200%) brightness(200%);
+        z-index: 1;
+
+        .lp-underglow-row {
+                height: 6%;
+                display: flex;
+                padding-left: 6%;
+                padding-right: 6%;
+                gap:1.5%;
+
+                .lp-underglow-led-parent {
+                width: 100%;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+        
+        .lp-underglow-column {
+                height: 100%;
+                width: 6%;
+                display: flex;
+                flex-direction: column;
+                gap: 1.5%;
+                padding-top: 0.5%;
+                padding-bottom: 0.5%;
+
+                .lp-underglow-led-parent {
+                height: 20%;
+                width: 100%;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+
+        :global(.lp-underglow-led-btn) {
+            padding: 0;
+            border: none;
+
+            height: 100%;
+            width: 100%;
+            background-color: rgba(255, 255, 255, 1.0);
+        }
+    }
+
+
     .lp-controls {
+        position: relative;
+        top: 0;
+        left: 0;
+
         height: 100%;
         width: 100%;
 
         display: flex;
-        gap: 1.5%;
+        // gap: 1.5%;
         flex-direction: column;
         padding: 3%;
+
+        z-index: 10;
 
         .lp-controls-row {
             height: 100%;
