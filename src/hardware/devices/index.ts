a6d3202cbef883851c93f2a33074b45b33afefea
Si Hyun Baek <62822977+sihyunlts@users.noreply.github.com>
2026-04-14 02:16:15 +0900

    fix(hardware): reorder device config list

--- src/hardware/devices/index.ts
+++ src/hardware/devices/index.ts
@@ -1,21 +1,21 @@
 import Mystrix from "./Mystrix";
-import LaunchpadMK2 from "./LaunchpadMK2"
+import LaunchpadProMK3 from "./LaunchpadProMK3";
 import LaunchpadX from "./LaunchpadX"
 import LaunchpadProMK2 from "./LaunchpadProMK2";
 import LaunchpadProMK2CFW from "./LaunchpadProMK2CFW";
-import LaunchpadProMK3 from "./LaunchpadProMK3";
 import LaunchpadS from "./LaunchpadS";
+import LaunchpadMK2 from "./LaunchpadMK2"
 import LaunchpadMiniMK3 from "./LaunchpadMiniMK3";
 import MidiFighter64 from "./MidiFighter64";
 
 export default {
   [Mystrix.name]: Mystrix,
-  [LaunchpadMK2.name]: LaunchpadMK2,
+  [LaunchpadProMK3.name]: LaunchpadProMK3,
   [LaunchpadX.name]: LaunchpadX,
   [LaunchpadProMK2.name]: LaunchpadProMK2,
   [LaunchpadProMK2CFW.name]: LaunchpadProMK2CFW,
-  [LaunchpadProMK3.name]: LaunchpadProMK3,
   [LaunchpadS.name]: LaunchpadS,
+  [LaunchpadMK2.name]: LaunchpadMK2,
   [LaunchpadMiniMK3.name]: LaunchpadMiniMK3,
   [MidiFighter64.name]: MidiFighter64,
 };
